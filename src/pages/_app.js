58aee6193ce104a8e97703a57f19a378c9eb0bb5
Israel Lazo <israel@deviantsfactions.com>
2023-08-08 11:19:01 +0100

    GTM

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,24 +1,47 @@
-import React from "react";
+import { React, useEffect } from "react";
 import Script from "next/script";
 import Head from "next/head";
 import "../styles/preloader.css";
 import "../styles/globals.css";
+import TagManager from "react-gtm-module";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-PLTJ8MPL" });
+  }, []);
+
   return (
     <>
       <Head>
         <title>Iteck</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
       </Head>
 
       <Component {...pageProps} />
 
-      <Script strategy="beforeInteractive" src="/assets/js/lib/pace.js"></Script>
-      <Script strategy="beforeInteractive" src="/assets/js/lib/bootstrap.bundle.min.js"></Script>
-      <Script strategy="beforeInteractive" src="/assets/js/lib/mixitup.min.js"></Script>
-      <Script strategy="beforeInteractive" src="/assets/js/lib/wow.min.js"></Script>
-      <Script strategy="beforeInteractive" src="/assets/js/lib/html5shiv.min.js"></Script>
+      <Script
+        strategy="beforeInteractive"
+        src="/assets/js/lib/pace.js"
+      ></Script>
+      <Script
+        strategy="beforeInteractive"
+        src="/assets/js/lib/bootstrap.bundle.min.js"
+      ></Script>
+      <Script
+        strategy="beforeInteractive"
+        src="/assets/js/lib/mixitup.min.js"
+      ></Script>
+      <Script
+        strategy="beforeInteractive"
+        src="/assets/js/lib/wow.min.js"
+      ></Script>
+      <Script
+        strategy="beforeInteractive"
+        src="/assets/js/lib/html5shiv.min.js"
+      ></Script>
       <Script strategy="lazyOnload" src="/assets/js/main.js"></Script>
     </>
   );
